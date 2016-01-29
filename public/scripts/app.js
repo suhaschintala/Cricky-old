ea5ed2eb743eb6053d5b579e0946da15c1828ce5
ark <arjunkrishna.iitm@gmail.com>
2016-01-30 00:15:56 +0530

    registration inclusive

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,19 @@
 angular.module('app',[])
 .controller('myCtrl',function($scope, $http){
+	$scope.isReg = false;
+	$scope.reg = function() {
+	/*	$http.post('url',{"name":$scope.name}).success(function(data) {
+			$scope.isReg = true;
+			console.log(data); //return id 	
+		});*/
+		$scope.isReg=true;	
+	};
 	$http.get('url')
 	.success(function(data){
 		console.log(data)
-	})
+	});
 	$scope.click = function(data) {
-		$http.post('url',{"pi":data}).success(function(rep){$scope.output='hello'});
+		$http.post('url',{"pi":data}).success(function(rep){$scope.output=data.flag;$scope.score=data.score});
 		
-	}
+	};
 })
